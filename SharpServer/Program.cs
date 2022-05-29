450147d404314fb9853a178f3bb74c731f21b6f7
coderadu <47945947+coderadu@users.noreply.github.com>
2022-05-29 11:59:41 +0300

    Add project files.

---
+++ SharpServer/Program.cs
@@ -0,0 +1,11 @@
+﻿using Server;
+public class SharpServer
+{
+    static void Main(string[] args)
+    {
+        int port = 3000;
+        if (args.Length > 0) port = int.Parse(args[0]);
+        else Console.WriteLine("You can specify a port: SharpServer.exe <port>");
+        WebServer server = new WebServer(port);
+    }
+}
---
+++ SharpServer/Server.cs
@@ -0,0 +1,40 @@
+﻿namespace Server
+{
+    using System;
+    using System.Net.Sockets;
+    using System.Threading;
+
+
+    class WebServer
+    {
+        private TcpListener _listener;
+        public WebServer(int port)
+        {
+            _listener = new TcpListener(port);
+            try
+            {
+                _listener.Start();
+                Console.WriteLine("Listening on: http://localhost:" + port);
+                Console.WriteLine("Connections made to the server will hang.");
+                Thread th = new Thread(new ThreadStart(Listen));
+                th.Start();
+
+            } catch(Exception e)
+            {
+                Console.WriteLine("Could not start server: " + e.ToString());
+            }
+
+        }
+        public void Listen()
+        {
+            while(true)
+            {
+                Socket socket = _listener.AcceptSocket();
+                if(socket.Connected)
+                {
+                    Console.WriteLine(socket.RemoteEndPoint);
+                }
+            }
+        }
+    }
+}
